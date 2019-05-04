ee081182ccdc7072f9c5459c2eb4af571e3cdefc
Julien Tschäppät <git@elendil.software>
2019-05-04 20:01:46 +0200

    Namespace correction

--- ASCOMStandard/Interfaces/ITelescope.cs
+++ ASCOMStandard/Interfaces/ITelescope.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ASCOM.Alpaca
+﻿namespace ASCOM.Alpaca.Interfaces
 {
     /// <summary>
     /// ASCOM Alpaca Telescope Interface
