3e80920e6319426f7b729e7627bcec3415e8c080
Daniel Van Noord <sg1mash98@gmail.com>
2020-04-04 15:23:46 -0400

    Split ICovCal status classes. Added ICovCal responses.

--- ASCOM.Standard.Types/Interfaces/Enums/CalibratorStatus.cs
+++ ASCOM.Standard.Types/Interfaces/Enums/CalibratorStatus.cs
@@ -4,39 +4,6 @@ using System.Text;
 
 namespace ASCOM.Alpaca.Interfaces
 {
-    public enum CoverStatus
-    {
-        /// <summary>
-        /// This device does not have a cover that can be closed independently 
-        /// </summary>
-        NotPresent = 0,
-
-        /// <summary>
-        /// The cover is closed
-        /// </summary>
-        Closed = 1,
-
-        /// <summary>
-        /// The cover is moving to a new position
-        /// </summary>
-        Moving = 2,
-
-        /// <summary>
-        /// The cover is open
-        /// </summary>
-        Open = 3,
-
-        /// <summary>
-        /// The state of the cover is unknown
-        /// </summary>
-        Unknown = 4,
-
-        /// <summary>
-        /// The device encountered an error when changing state
-        /// </summary>
-        Error = 5
-    }
-
     /// <summary>
     /// Describes the state of a calibration device
     /// </summary>
