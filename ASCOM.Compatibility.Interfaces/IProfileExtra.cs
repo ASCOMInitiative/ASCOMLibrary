64a264ac34c6cc73db48dfed029eb8a8f6c0cf7f
Daniel Van Noord <sg1mash98@gmail.com>
2020-04-25 14:54:25 -0400

    Began adding compatibility interfaces.

---
+++ ASCOM.Compatibility.Interfaces/IProfileExtra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Compatibility.Interfaces
+{
+    public interface IProfileExtra
+    {
+        void MigrateProfile(string CurrentPlatformVersion);
+
+        void DeleteValue(string DriverID, string Name);
+
+        string GetValue(string DriverID, string Name);
+
+        string GetValue(string DriverID, string Name, string SubKey);
+
+        ArrayList SubKeys(string DriverID);
+
+        ArrayList Values(string DriverID);
+
+        void WriteValue(string DriverID, string Name, string Value);
+
+        //ASCOMProfile GetProfile(string DriverId);
+
+        //void SetProfile(string DriverId, ASCOMProfile XmlProfileKey);
+    }
+}
---
+++ ASCOM.Compatibility.Interfaces/ITraceLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Compatibility.Interfaces
+{
+    public interface ITraceLogger
+    {
+        void LogStart(string Identifier, string Message);
+
+        void LogContinue(string Message, bool HexDump);
+
+        void LogFinish(string Message, bool HexDump);
+
+        void LogMessage(string Identifier, string Message, bool HexDump);
+
+        bool Enabled { get; set; }
+
+        void LogIssue(string Identifier, string Message);
+
+        void SetLogFile(string LogFileName, string LogFileType);
+
+        void BlankLine();
+
+        string LogFileName { get; }
+
+        void LogMessageCrLf(string Identifier, string Message);
+
+        string LogFilePath { get; set; }
+
+        int IdentifierWidth { get; set; }
+    }
+}
