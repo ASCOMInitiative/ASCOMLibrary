b427983550b7e34ac63ecff421c4995eecd12730
Daniel Van Noord <sg1mash98@gmail.com>
2020-09-06 09:37:46 -0400

    Added the ASCOM Utilities Compatibility library.

---
+++ ASCOM.Compatibility.Utilities/TraceLoggerCompat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCOM.Compatibility.Utilities
+{
+    public class TraceLoggerCompat : ASCOM.Utilities.TraceLogger, ASCOM.Standard.Interfaces.ITraceLogger
+    {
+        public TraceLoggerCompat() : base()
+        {
+
+        }
+
+        public TraceLoggerCompat(string LogFileType) : base(LogFileType)
+        {
+
+        }
+
+        public TraceLoggerCompat(string LogFileName, string LogFileType) : base(LogFileName, LogFileType)
+        {
+
+        }
+    }
+}
