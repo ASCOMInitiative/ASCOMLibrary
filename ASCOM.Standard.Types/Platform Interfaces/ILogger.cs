06302a51ead4ca1ceb5e1249973f37fb74e48e68
Daniel Van Noord <sg1mash98@gmail.com>
2020-10-05 22:00:38 -0400

    Began adding test log interfaces, enums and extensions

---
+++ ASCOM.Standard.Types/Platform Interfaces/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Standard.Interfaces
+{
+    public interface ILogger
+    {
+        LogLevel LoggingLevel
+        {
+            get;
+        }
+
+        void SetMinimumLoggingLevel(LogLevel level);
+
+        void Log(LogLevel level, string message);
+    }
+}
