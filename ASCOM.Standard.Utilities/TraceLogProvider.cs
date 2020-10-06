f3adfbd8f854053cd1a2cd5036e192e2a3520f8e
Daniel Van Noord <sg1mash98@gmail.com>
2020-10-05 22:23:46 -0400

    Added a static logging class and a wrapper around tracelogger

---
+++ ASCOM.Standard.Utilities/TraceLogProvider.cs
@@ -0,0 +1,40 @@
+﻿using ASCOM.Standard.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Standard.Utilities
+{
+    public class TraceLogProvider : ILogger
+    {
+        private TraceLogger logger;
+
+        public TraceLogProvider()
+        {
+            logger = new TraceLogger();
+        }
+
+        public TraceLogProvider(string LogFileType)
+        {
+            logger = new TraceLogger(null, LogFileType);
+        }
+        public LogLevel LoggingLevel
+        {
+            get;
+            private set;
+        } = LogLevel.Information;
+
+        public void Log(LogLevel level, string message)
+        {
+            if (this.IsLevelActive(level))
+            {
+                logger.LogMessage($"[{level}]", message);
+            }
+        }
+
+        public void SetMinimumLoggingLevel(LogLevel level)
+        {
+            LoggingLevel = level;
+        }
+    }
+}
