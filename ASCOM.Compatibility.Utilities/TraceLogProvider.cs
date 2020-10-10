fc6fe3fa0886590cbb8b32f5830c776f0df53947
Daniel Van Noord <sg1mash98@gmail.com>
2020-10-09 20:02:43 -0400

    Added a Compatibility provider

---
+++ ASCOM.Compatibility.Utilities/TraceLogProvider.cs
@@ -0,0 +1,40 @@
+﻿using ASCOM.Standard.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Compatibility.Utilities
+{
+    public class TraceLogProvider : ILogger
+    {
+        private readonly ASCOM.Utilities.TraceLogger logger;
+
+        public TraceLogProvider()
+        {
+            logger = new ASCOM.Utilities.TraceLogger();
+        }
+
+        public TraceLogProvider(string LogFileType)
+        {
+            logger = new ASCOM.Utilities.TraceLogger(null, LogFileType);
+        }
+        public LogLevel LoggingLevel
+        {
+            get;
+            private set;
+        } = LogLevel.Information;
+
+        public void Log(LogLevel level, string message)
+        {
+            if (this.IsLevelActive(level))
+            {
+                logger.LogMessage($"[{level}]", message);
+            }
+        }
+
+        public void SetMinimumLoggingLevel(LogLevel level)
+        {
+            LoggingLevel = level;
+        }
+    }
+}
