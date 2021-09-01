b26d3f8f7ded76a985b0afe78f6acc8bcd48ff11
Daniel Van Noord <sg1mash98@gmail.com>
2021-08-31 21:00:43 -0400

    Added COM Driver Access to ASCOM Standard

---
+++ ASCOM.Standard.COM.DriverAccess/CoverCalibrator.cs
@@ -0,0 +1,50 @@
+﻿using ASCOM.Standard.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Standard.COM.DriverAccess
+{
+    public class CoverCalibrator : ASCOMDevice, ASCOM.Standard.Interfaces.ICoverCalibratorV1
+    {
+        public static List<ASCOMRegistration> CoverCalibrators => ProfileAccess.GetDrivers(DriverTypes.CoverCalibrator);
+
+        public CoverCalibrator(string ProgID) : base(ProgID)
+        {
+
+        }
+
+        public CoverStatus CoverState => (CoverStatus) base.Device.CoverState;
+
+        public CalibratorStatus CalibratorState => (CalibratorStatus) base.Device.CalibratorState;
+
+        public int Brightness => base.Device.Brightness;
+
+        public int MaxBrightness => base.Device.MaxBrightness;
+
+        public void OpenCover()
+        {
+            base.Device.OpenCover();
+        }
+
+        public void CloseCover()
+        {
+            base.Device.CloseCover();
+        }
+
+        public void HaltCover()
+        {
+            base.Device.HaltCover();
+        }
+
+        public void CalibratorOn(int Brightness)
+        {
+            base.Device.CalibratorOn(Brightness);
+        }
+
+        public void CalibratorOff()
+        {
+            base.Device.CalibratorOff();
+        }
+    }
+}
---
+++ ASCOM.Standard.COM.DriverAccess/Profile/DriverTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Standard.COM.DriverAccess
+{
+    /// <summary>
+    /// The set of Driver types as of ASCOM 6.5. Rather then directly using strings these are used to request specific types.
+    /// </summary>
+    public enum DriverTypes
+    {
+        Camera,
+        CoverCalibrator,
+        Dome,
+        FilterWheel,
+        Focuser,
+        ObservingConditions,
+        Rotator,
+        SafetyMonitor,
+        Switch,
+        Telescope,
+        Video
+    }
+}
