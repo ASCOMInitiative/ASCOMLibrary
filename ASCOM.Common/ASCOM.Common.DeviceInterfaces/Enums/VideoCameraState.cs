f6a58f5f7a0877a65ab39bf0115ecf2e9f577339
Daniel Van Noord <sg1mash98@gmail.com>
2022-03-03 21:16:46 -0500

    Added video specific enums and interfaces

---
+++ ASCOM.Common/ASCOM.Common.DeviceInterfaces/Enums/VideoCameraState.cs
@@ -0,0 +1,23 @@
+﻿namespace ASCOM.Common.DeviceInterfaces
+{
+    /// <summary>
+    /// ASCOM Video Camera status values.
+    /// </summary>
+    public enum VideoCameraState
+    {
+        /// <summary>
+        /// Camera status running. The video is receiving signal and video frames are available for viewing or recording.
+        /// </summary>
+        Running = 0,
+
+        /// <summary>
+        /// Camera status recording. The video camera is recording video to the file system. Video frames are available for viewing.
+        /// </summary>
+        Recording = 1,
+
+        /// <summary>
+        /// Camera status error. The video camera is in a state of an error and cannot continue its operation. Usually a reset will be required to resolve the error condition.
+        /// </summary>
+        Error = 2
+    }
+}
---
+++ ASCOM.Common/ASCOM.Common.DeviceInterfaces/IVideoFrame.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// <summary>Defines the IVideoFrame Interface</summary>
+// -----------------------------------------------------------------------
+namespace ASCOM.Common.DeviceInterfaces
+{
+    /// <summary>
+    /// Defines the IVideoFrame Interface
+    /// Note: this applies to COM only
+    /// </summary>
+    public interface IVideoFrame
+    {
+    }
+}
