6f30c0ea49366294c008e15014369a212de0d28c
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2022-10-07 17:13:12 +0100

    Add missing XML help to ITraceLogger.

--- ASCOM.Common/ASCOM.Common.Interfaces/ITraceLogger.cs
+++ ASCOM.Common/ASCOM.Common.Interfaces/ITraceLogger.cs
@@ -11,16 +11,36 @@ namespace ASCOM.Common.Interfaces
     /// <remarks>Inherits the ILogger interface and exposed the </remarks>
     public interface ITraceLogger : ILogger
     {
+        /// <summary>
+        /// Log a message to the logger device
+        /// </summary>
+        /// <param name="method">Name of the initiating method</param>
+        /// <param name="message">Message to log</param>
         void LogMessage(string method, string message);
 
+        /// <summary>
+        /// Create a blank line in the log
+        /// </summary>
         void BlankLine();
 
+        /// <summary>
+        /// Enable or disable the logger
+        /// </summary>
         bool Enabled { get; set; }
 
+        /// <summary>
+        /// Set the width in characters of the identifier / method name column in the log file
+        /// </summary>
         int IdentifierWidth { get; set; }
 
+        /// <summary>
+        /// False to log local times, true to log UTC times, 
+        /// </summary>
         bool UseUtcTime { get; set; }
 
+        /// <summary>
+        /// True to honour CrLf sequences (a log message could occupy one or more lines in the log), False to print CrLf as [Cr]{Lf} ensuring that each message only occupies one line in the log.
+        /// </summary>
         bool RespectCrLf { get; set; }
     }
 }
