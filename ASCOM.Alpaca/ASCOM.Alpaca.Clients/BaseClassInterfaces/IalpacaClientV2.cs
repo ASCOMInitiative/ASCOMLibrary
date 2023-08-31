fcc045733b38eda4a6e7b1658d746d9e4a6084c0
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2023-08-31 13:16:26 +0100

    Add an interface for the Alpaca device base class.

---
+++ ASCOM.Alpaca/ASCOM.Alpaca.Clients/BaseClassInterfaces/IalpacaClientV2.cs
@@ -0,0 +1,19 @@
+﻿using ASCOM.Alpaca.Clients;
+using ASCOM.Common.DeviceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASCOM.Alpaca.Clients
+{
+    /// <summary>
+    /// Interface to expose the common Platform 7 ASCOM methods plus the Alpaca device configuration property
+    /// </summary>
+    public interface IAlpacaClientV2 : IAscomDeviceV2
+    {
+        /// <summary>
+        /// Client configuration state
+        /// </summary>
+        ClientConfiguration ClientConfiguration { get; }
+    }
+}
