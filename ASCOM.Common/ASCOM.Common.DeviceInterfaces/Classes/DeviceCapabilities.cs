6874882706418f3b7488b82d05b6bb9eab07c45e
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2024-04-05 16:31:50 +0100

    Add further checks to DeviceCapabilities class.

--- ASCOM.Common/ASCOM.Common.DeviceInterfaces/Classes/DeviceCapabilities.cs
+++ ASCOM.Common/ASCOM.Common.DeviceInterfaces/Classes/DeviceCapabilities.cs
@@ -11,22 +11,22 @@ namespace ASCOM.Common.DeviceInterfaces
         /// Returns <see langword="true"/> for all devices except IFocuserV1 devices that do not have the Connected property
         /// </summary>
         /// <param name="deviceType">Device type.</param>
-        /// <param name="driverInterfaceVersion">Interface version of this driver (Int16)</param>
+        /// <param name="interfaceVersion">Interface version of this device (Int16)</param>
         /// <returns>True for all device interfaces except IFocuserV1</returns>
         /// <exception cref="InvalidValueException"></exception>
-        public static bool HasConnected(DeviceTypes? deviceType, short driverInterfaceVersion)
+        public static bool HasConnected(DeviceTypes? deviceType, short interfaceVersion)
         {
-            return HasConnected(deviceType, Convert.ToInt32(driverInterfaceVersion));
+            return HasConnected(deviceType, Convert.ToInt32(interfaceVersion));
         }
 
         /// <summary>
         /// Returns <see langword="true"/> for all devices except IFocuserV1 devices that do not have the Connected property
         /// </summary>
         /// <param name="deviceType">Device type.</param>
-        /// <param name="driverInterfaceVersion">Interface version of this driver (Int32)</param>
+        /// <param name="interfaceVersion">Interface version of this device (Int32)</param>
         /// <returns>True for all device interfaces except IFocuserV1</returns>
         /// <exception cref="InvalidValueException"></exception>
-        public static bool HasConnected(DeviceTypes? deviceType, int driverInterfaceVersion)
+        public static bool HasConnected(DeviceTypes? deviceType, int interfaceVersion)
         {
             // Throw an exception if no device type is supplied
             if (!deviceType.HasValue)
@@ -37,7 +37,7 @@ namespace ASCOM.Common.DeviceInterfaces
             {
                 // Focuser only has Connected in IFocuserV2 and later
                 case DeviceTypes.Focuser: // Focuser device
-                    if (driverInterfaceVersion ==1) // IFocuserV1 so return false
+                    if (interfaceVersion ==1) // IFocuserV1 so return false
                         return false;
                     else // IFocuserV2 or later so return true
                         return true;
@@ -51,108 +51,138 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <summary>
         /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports asynchronous Switch methods
         /// </summary>
-        /// <param name="driverInterfaceVersion">Interface version of this driver (Int16)</param>
+        /// <param name="interfaceVersion">Interface version of this device (Int16)</param>
         /// <returns>True when the interface version supports AsyncSwitch methods.</returns>
         /// <exception cref="InvalidValueException">The supplied interface version is 0 or less.</exception>
-        public static bool HasAsyncSwitch(int driverInterfaceVersion)
+        public static bool HasAsyncSwitch(int interfaceVersion)
         {
             // Validate parameter
-            if (driverInterfaceVersion < 1)
-                throw new InvalidValueException($"ASCOMLibrary.DeviceCapabilities.HasAsyncSwitch - Supplied interface version is 0 or negative: {driverInterfaceVersion}");
+            if (interfaceVersion < 1)
+                throw new InvalidValueException($"ASCOMLibrary.DeviceCapabilities.HasAsyncSwitch - Supplied interface version is 0 or negative: {interfaceVersion}");
 
-            return driverInterfaceVersion >= 3;
+            return interfaceVersion >= 3;
         }
 
         /// <summary>
-        /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports Connect / Disconnect and DeviceState
+        /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports the CoverCalibrator.CalibratorChanging property
         /// </summary>
-        /// <param name="deviceType">Device type.</param>
-        /// <param name="driverInterfaceVersion">Interface version of this driver (Int16)</param>
-        /// <returns>True when the interface version supports Connect / Disconnect</returns>
-        public static bool HasConnectAndDeviceState(DeviceTypes? deviceType, short driverInterfaceVersion)
+        /// <param name="interfaceVersion">Interface version of this device</param>
+        /// <returns>True when the interface version supports CoverCalibrator.CalibratorChanging.</returns>
+        /// <exception cref="InvalidValueException">The supplied interface version is 0 or less.</exception>
+        public static bool HasCalibratorChanging(int interfaceVersion)
         {
-            return HasConnectAndDeviceState(deviceType, Convert.ToInt32(driverInterfaceVersion));
+            // Validate parameter
+            if (interfaceVersion < 1)
+                throw new InvalidValueException($"ASCOMLibrary.DeviceCapabilities.HasCalibratorChanging - Supplied interface version is 0 or negative: {interfaceVersion}");
+
+            return interfaceVersion >= 2;
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports the CoverCalibrator.HasCoverMoving property
+        /// </summary>
+        /// <param name="interfaceVersion">Interface version of this device</param>
+        /// <returns>True when the interface version supports CoverCalibrator.HasCoverMoving.</returns>
+        /// <exception cref="InvalidValueException">The supplied interface version is 0 or less.</exception>
+        public static bool HasCoverMoving(int interfaceVersion)
+        {
+            // Validate parameter
+            if (interfaceVersion < 1)
+                throw new InvalidValueException($"ASCOMLibrary.DeviceCapabilities.HasCoverMoving - Supplied interface version is 0 or negative: {interfaceVersion}");
+
+            return interfaceVersion >= 2;
         }
 
         /// <summary>
         /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports Connect / Disconnect and DeviceState
         /// </summary>
         /// <param name="deviceType">Device type.</param>
-        /// <param name="driverInterfaceVersion">Interface version of this driver (Int32)</param>
+        /// <param name="interfaceVersion">Interface version of this device (Int16)</param>
         /// <returns>True when the interface version supports Connect / Disconnect</returns>
-        public static bool HasConnectAndDeviceState(DeviceTypes? deviceType, int driverInterfaceVersion)
+        public static bool HasConnectAndDeviceState(DeviceTypes? deviceType, short interfaceVersion)
+        {
+            return HasConnectAndDeviceState(deviceType, Convert.ToInt32(interfaceVersion));
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the device has a Platform 7 or later interface that supports Connect / Disconnect and DeviceState
+        /// </summary>
+        /// <param name="deviceType">Device type.</param>
+        /// <param name="interfaceVersion">Interface version of this device (Int32)</param>
+        /// <returns>True when the interface version supports Connect / Disconnect</returns>
+        public static bool HasConnectAndDeviceState(DeviceTypes? deviceType, int interfaceVersion)
         {
             if (!deviceType.HasValue)
             {
                 throw new InvalidValueException("ASCOMLibrary.DeviceCapabilities.HasConnectAndDeviceState - Supplied device type is null.");
             }
 
-            // Switch on the type of this DriverAccess object
+            // Switch on the type of this device
             switch (deviceType)
             {
                 // True if interface version is greater than 3
                 case DeviceTypes.Camera:
-                    if (driverInterfaceVersion > 3)
+                    if (interfaceVersion > 3)
                         return true;
                     break;
 
                 // True if interface version is greater than 1
                 case DeviceTypes.CoverCalibrator:
-                    if (driverInterfaceVersion > 1)
+                    if (interfaceVersion > 1)
                         return true;
                     break;
 
                 // True if interface version is greater than 2
                 case DeviceTypes.Dome:
-                    if (driverInterfaceVersion > 2)
+                    if (interfaceVersion > 2)
                         return true;
                     break;
 
                 // True if interface version is greater than 2
                 case DeviceTypes.FilterWheel:
-                    if (driverInterfaceVersion > 2)
+                    if (interfaceVersion > 2)
                         return true;
                     break;
 
                 // True if interface version is greater than 3
                 case DeviceTypes.Focuser:
-                    if (driverInterfaceVersion > 3)
+                    if (interfaceVersion > 3)
                         return true;
                     break;
 
                 // True if interface version is greater than 1
                 case DeviceTypes.ObservingConditions:
-                    if (driverInterfaceVersion > 1)
+                    if (interfaceVersion > 1)
                         return true;
                     break;
 
                 // True if interface version is greater than 3
                 case DeviceTypes.Rotator:
-                    if (driverInterfaceVersion > 3)
+                    if (interfaceVersion > 3)
                         return true;
                     break;
 
                 // True if interface version is greater than 1
                 case DeviceTypes.SafetyMonitor:
-                    if (driverInterfaceVersion > 2)
+                    if (interfaceVersion > 2)
                         return true;
                     break;
 
                 // True if interface version is greater than 2
                 case DeviceTypes.Switch:
-                    if (driverInterfaceVersion > 2)
+                    if (interfaceVersion > 2)
                         return true;
                     break;
 
                 // True if interface version is greater than 3
                 case DeviceTypes.Telescope:
-                    if (driverInterfaceVersion > 3)
+                    if (interfaceVersion > 3)
                         return true;
                     break;
 
                 // True if interface version is greater than 1
                 case DeviceTypes.Video:
-                    if (driverInterfaceVersion > 1)
+                    if (interfaceVersion > 1)
                         return true;
                     break;
 
