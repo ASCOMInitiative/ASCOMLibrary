c08d5daa817a8a82500eeb05e4c8f14b34f00a85
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2024-04-16 14:23:47 +0100

    ChooserSA - Remove serialisation initiators that are not required and that have started generated compiler warnings.

--- ASCOM.Com.ChooserSA/Exceptions.cs
+++ ASCOM.Com.ChooserSA/Exceptions.cs
@@ -27,22 +27,12 @@ namespace ASCOM.Com.Exceptions
         public HelperException(string message, Exception inner) : base(message, inner)
         {
         }
-
-        /// <summary>
-        /// Serialise the exception
-        /// </summary>
-        /// <param name="info">Serialisation information</param>
-        /// <param name="context">Serialisation context</param>
-        /// <remarks></remarks>
-        public HelperException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
-        {
-        }
     }
 
     /// <summary>
     /// Exception thrown if there is any problem in reading or writing the profile from or to the file system
     /// </summary>
-    /// <remarks>This is an ifrastructural exception indicatig that there is a problem at the file access layer
+    /// <remarks>This is an infrastructural exception indicating that there is a problem at the file access layer
     /// in the profile code. Possible underlying reasons are security access permissions, file system full and hardware failure.
     /// </remarks>
     public class ProfilePersistenceException : HelperException
@@ -63,16 +53,6 @@ namespace ASCOM.Com.Exceptions
         /// <param name="inner">Exception to be reported as the inner exception</param>
         /// <remarks></remarks>
         public ProfilePersistenceException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        /// <summary>
-        /// Serialise the exception
-        /// </summary>
-        /// <param name="info">Serialisation information</param>
-        /// <param name="context">Serialisation context</param>
-        /// <remarks></remarks>
-        public ProfilePersistenceException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
         {
         }
     }
@@ -100,16 +80,6 @@ namespace ASCOM.Com.Exceptions
         /// <param name="inner">Exception to be reported as the inner exception</param>
         /// <remarks></remarks>
         public DriverNotRegisteredException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        /// <summary>
-        /// Serialise the exception
-        /// </summary>
-        /// <param name="info">Serialisation information</param>
-        /// <param name="context">Serialisation context</param>
-        /// <remarks></remarks>
-        public DriverNotRegisteredException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
         {
         }
     }
