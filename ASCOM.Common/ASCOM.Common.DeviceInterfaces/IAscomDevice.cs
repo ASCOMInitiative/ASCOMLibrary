15994f4e7b547799aa2423359942f106d4295d2d
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-02-03 12:20:37 +0000

    Interfaces - Update remarks for common interface members to refer to the canonical definitions.

--- ASCOM.Common/ASCOM.Common.DeviceInterfaces/IAscomDevice.cs
+++ ASCOM.Common/ASCOM.Common.DeviceInterfaces/IAscomDevice.cs
@@ -24,25 +24,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
         /// <exception cref="ActionNotImplementedException">It is intended that the <see cref="SupportedActions"/> method will inform clients of driver capabilities, but the driver must still throw 
 		/// an <see cref="ASCOM.ActionNotImplementedException"/> exception  if it is asked to perform an action that it does not support.</exception>
-        /// <example>Suppose filter wheels start to appear with automatic wheel changers; new actions could 
-        /// be “FilterWheel:QueryWheels” and “FilterWheel:SelectWheel”. The former returning a 
-        /// formatted list of wheel names and the second taking a wheel name and making the change, returning appropriate 
-        /// values to indicate success or failure.
-        /// </example>
-        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> 
-        /// This method is intended for use in all current and future device types and to avoid name clashes, management of action names 
-        /// is important from day 1. A two-part naming convention will be adopted - <b>DeviceType:UniqueActionName</b> where:
-        /// <list type="bullet">
-        /// <item><description>DeviceType is the string name of the device type e.g. Telescope, Camera, Switch etc.</description></item>
-        /// <item><description>UniqueActionName is a single word, or multiple words joined by underscore characters, that sensibly describes the action to be performed.</description></item>
-        /// </list>
-        /// <para>
-        /// It is recommended that UniqueActionNames should be a maximum of 16 characters for legibility.
-        /// Should the same function and UniqueActionName be supported by more than one type of device, the reserved DeviceType of 
-        /// “General” will be used. Action names will be case insensitive, so FilterWheel:SelectWheel, filterwheel:selectwheel 
-        /// and FILTERWHEEL:SELECTWHEEL will all refer to the same action.</para>
-        /// <para>The names of all supported actions must be returned in the <see cref="SupportedActions"/> property.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.Action">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string Action(string actionName, string actionParameters);
 
         /// <summary>
@@ -57,13 +40,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <exception cref="NotImplementedException">If the method is not implemented</exception>
         /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>May throw a NotImplementedException.</b></p>
-        /// <para>The CommandXXX methods are a historic mechanic that provides clients with direct and unimpeded access to change device hardware configuration. While highly enabling for clients, this mechanic is inherently risky
-        /// because clients can fundamentally change hardware operation without the driver being aware that a change is taking / has taken place.</para>
-        /// <para>The newer Action / SupportedActions mechanic provides discrete, named, functions that can deliver any functionality required.They do need driver authors to make provision for them within the 
-        /// driver, but this approach is much lower risk than using the CommandXXX methods because it enables the driver to resolve conflicts between standard device interface commands and extended commands 
-        /// provided as Actions.The driver is always aware of what is happening and can adapt more effectively to client needs.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.CommandBlind">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         void CommandBlind(string command, bool raw = false);
 
         /// <summary>
@@ -81,13 +59,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <exception cref="NotImplementedException">If the method is not implemented</exception>
         /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>May throw a NotImplementedException.</b></p>
-        /// <para>The CommandXXX methods are a historic mechanic that provides clients with direct and unimpeded access to change device hardware configuration. While highly enabling for clients, this mechanic is inherently risky
-        /// because clients can fundamentally change hardware operation without the driver being aware that a change is taking / has taken place.</para>
-        /// <para>The newer Action / SupportedActions mechanic provides discrete, named, functions that can deliver any functionality required.They do need driver authors to make provision for them within the 
-        /// driver, but this approach is much lower risk than using the CommandXXX methods because it enables the driver to resolve conflicts between standard device interface commands and extended commands 
-        /// provided as Actions.The driver is always aware of what is happening and can adapt more effectively to client needs.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.CommandBool">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         bool CommandBool(string command, bool raw = false);
 
         /// <summary>
@@ -105,13 +78,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <exception cref="NotImplementedException">If the method is not implemented</exception>
         /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>May throw a NotImplementedException.</b></p>
-        /// <para>The CommandXXX methods are a historic mechanic that provides clients with direct and unimpeded access to change device hardware configuration. While highly enabling for clients, this mechanic is inherently risky
-        /// because clients can fundamentally change hardware operation without the driver being aware that a change is taking / has taken place.</para>
-        /// <para>The newer Action / SupportedActions mechanic provides discrete, named, functions that can deliver any functionality required.They do need driver authors to make provision for them within the 
-        /// driver, but this approach is much lower risk than using the CommandXXX methods because it enables the driver to resolve conflicts between standard device interface commands and extended commands 
-        /// provided as Actions.The driver is always aware of what is happening and can adapt more effectively to client needs.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.CommandString">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string CommandString(string command, bool raw = false);
 
         /// <summary>
@@ -120,9 +88,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// </summary>
         /// <value><c>true</c> if connected; otherwise, <c>false</c>.</value>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <para>Drivers / Devices must not throw a <see cref="NotConnectedException"/> here, that exception is for use in other methods that require a connection in order to succeed.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.Connected">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         bool Connected { get; set; }
 
         /// <summary>
@@ -131,50 +98,40 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <value>The description.</value>
         /// <exception cref="NotConnectedException">When <see cref="Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. The device did not successfully complete the request.</exception> 
-        /// <remarks>
-        /// Any ASCII characters may be used. The string must not exceed 68 characters (for compatibility with FITS headers).
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.Description">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string Description { get; }
 
         /// <summary>
         /// Descriptive and version information about this ASCOM driver.
         /// </summary>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. The device did not successfully complete the request.</exception> 
-        /// <remarks>
-        /// This string may contain line endings and may be hundreds to thousands of characters long.
-        /// It is intended to display detailed information on the ASCOM driver, including version and copyright data.
-        /// See the Description property for descriptive info on the telescope itself.
-        /// To get the driver version in a parseable string, use the DriverVersion property.
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.DriverInfo">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string DriverInfo { get; }
 
         /// <summary>
         /// A string in the form "n.n" containing only the major and minor version of the driver.
         /// </summary>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. The device did not successfully complete the request.</exception> 
-        /// <remarks>
-        /// Not to be confused with the InterfaceVersion property, which is the version of this specification supported by the driver (currently 2). 
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.DriverVersion">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string DriverVersion { get; }
 
         /// <summary>
         /// The interface version number that this device implements.
         /// </summary>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <para>E.g. a device that implements IDeviceV3 should return 3.</para>
-        /// <para>
-        /// Clients can detect legacy V1 drivers by trying to read ths property.
-        /// If the driver raises an error, it is a V1 driver. V1 did not specify this property. A driver may also return a value of 1. 
-        /// In other words, a raised error or a return value of 1 indicates that the driver is a V1 driver. 
-        /// </para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.InterfaceVersion">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         short InterfaceVersion { get; }
 
         /// <summary>
         /// The short name of the driver, for display purposes
         /// </summary>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.Name">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         string Name { get; }
 
         /// <summary>
@@ -183,12 +140,8 @@ namespace ASCOM.Common.DeviceInterfaces
         /// <value>An ArrayList of strings (SafeArray collection) containing the names of supported actions.</value>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Must be implemented</b></p> This method must return an empty <see cref="IList{String}"/> object if no actions are supported. Please do not throw a <see cref="NotImplementedException" />.
-        /// <para>SupportedActions is a "discovery" mechanism that enables clients to know which Actions a device supports without having to exercise the Actions themselves. This mechanism is necessary because there could be
-        /// people / equipment safety issues if actions are called unexpectedly or out of a defined process sequence.
-        /// It follows from this that SupportedActions must return names that match the spelling of Action names exactly, without additional descriptive text. However, returned names may use any casing
-        /// because the <see cref="Action" /> ActionName parameter is case insensitive.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/camera.html#Camera.SupportedActions">Canonical definition.</see>.
+        /// <para><b>Please Note</b>: The Camera definition in the link applies to all device types.</para></remarks>
         IList<string> SupportedActions { get; }
 
         /// <summary>
