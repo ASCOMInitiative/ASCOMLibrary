110e9bfe3ee16aced776d268c4e6fcdf80853686
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-02-03 08:50:23 +0000

    Interfaces - Update FilterWheel and Focuser remarks to point at canonical definitions.

--- ASCOM.Common/ASCOM.Common.DeviceInterfaces/IFocuserV3.cs
+++ ASCOM.Common/ASCOM.Common.DeviceInterfaces/IFocuserV3.cs
@@ -16,7 +16,7 @@
         /// </summary>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Must be implemented</b></p> </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.Absolute">Canonical definition</see></remarks>
         bool Absolute { get; }
 
         /// <summary>
@@ -25,11 +25,7 @@
         /// <exception cref="NotImplementedException">Focuser does not support this method.</exception>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red"><b>Can throw a not implemented exception</b></p>Some focusers may not support this function, in which case an exception will be raised. 
-        /// <para><b>Recommendation:</b> Host software should call this method upon initialization and,
-        /// if it fails, disable the Halt button in the user interface.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.Halt">Canonical definition</see></remarks>
         void Halt();
 
         /// <summary>
@@ -37,7 +33,7 @@
         /// </summary>
         /// <exception cref="NotConnectedException">If the driver is not connected.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Must be implemented</b></p></remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.IsMoving">Canonical definition</see></remarks>
         bool IsMoving { get; }
 
         /// <summary>
@@ -46,10 +42,7 @@
         /// </summary>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red"><b>Must be implemented</b></p>
-        /// For most focusers this is the same as the <see cref="MaxStep" /> property. This is normally used to limit the Increment display in the host software.
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.MaxIncrement">Canonical definition</see></remarks>
         int MaxIncrement { get; }
 
         /// <summary>
@@ -57,10 +50,7 @@
         /// </summary>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red"><b>Must be implemented</b></p>
-        /// The focuser can step between 0 and <see cref="MaxStep" />. If an attempt is made to move the focuser beyond these limits, it will automatically stop at the limit.
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.MaxStep">Canonical definition</see></remarks>
         int MaxStep { get; }
 
         /// <summary>
@@ -69,19 +59,7 @@
         /// <param name="Position">Step distance or absolute position, depending on the value of the <see cref="Absolute" /> property.</param>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Must be implemented</b></p>
-        /// <para>If the <see cref="Absolute" /> property is True, then this is an absolute positioning focuser. The <see cref="Move">Move</see> command tells the focuser to move to an exact step position, and the Position parameter 
-        /// of the <see cref="Move">Move</see> method is an integer between 0 and <see cref="MaxStep" />.</para>
-        /// <para>If the <see cref="Absolute" /> property is False, then this is a relative positioning focuser. The <see cref="Move">Move</see> command tells the focuser to move in a relative direction, and the Position parameter 
-        /// of the <see cref="Move">Move</see> method (in this case, step distance) is an integer between minus <see cref="MaxIncrement" /> and plus <see cref="MaxIncrement" />.</para>
-        /// <para><b>BEHAVIOURAL CHANGE - Platform 6.4</b></para>
-        /// <para>Prior to Platform 6.4, the interface specification mandated that drivers must throw an <see cref="InvalidOperationException"/> if a move was attempted when <see cref="TempComp"/> was True, even if the focuser 
-        /// was able to execute the move safely without disrupting temperature compensation.</para>
-        /// <para>Following discussion on ASCOM-Talk in January 2018, the Focuser interface specification has been revised to IFocuserV3, removing the requirement to throw the InvalidOperationException exception. IFocuserV3 compliant drivers 
-        /// are expected to execute Move requests when temperature compensation is active and to hide any specific actions required by the hardware from the client. For example this could be achieved by disabling temperature compensation, moving the focuser and re-enabling 
-        /// temperature compensation or simply by moving the focuser with compensation enabled if the hardware supports this.</para>
-        /// <para>Conform will continue to pass IFocuserV2 drivers that throw InvalidOperationException exceptions. However, Conform will now fail IFocuserV3 drivers that throw InvalidOperationException exceptions, in line with this revised specification.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.Move">Canonical definition</see></remarks>
         void Move(int Position);
 
         /// <summary>
@@ -90,10 +68,7 @@
         /// <exception cref="NotImplementedException">If the property is not available for this device.</exception>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red"><b>Can throw a not implemented exception</b></p> Valid only for absolute positioning focusers (see the <see cref="Absolute" /> property).
-        /// A <see cref="NotImplementedException">NotImplementedException</see> exception must be thrown if this device is a relative positioning focuser rather than an absolute position focuser.
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.Position">Canonical definition</see></remarks>
         int Position { get; }
 
         /// <summary>
@@ -102,7 +77,7 @@
         /// <exception cref= "NotImplementedException">If the focuser does not intrinsically know what the step size is.</exception>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> Must throw an exception if the focuser does not intrinsically know what the step size is.</remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.StepSize">Canonical definition</see></remarks>
         double StepSize { get; }
 
         /// <summary>
@@ -111,20 +86,7 @@
         /// <exception cref="NotImplementedException">If <see cref="TempCompAvailable" /> is False and an attempt is made to set <see cref="TempComp" /> to true.</exception>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red;margin-bottom:0"><b>TempComp Read must be implemented and must not throw a NotImplementedException. </b></p>
-        /// <p style="color:red;margin-top:0"><b>TempComp Write can throw a NotImplementedException.</b></p>
-        /// If the <see cref="TempCompAvailable" /> property is True, then setting <see cref="TempComp" /> to True puts the focuser into temperature tracking mode; setting it to False will turn off temperature tracking.
-        /// <para>If temperature compensation is not available, this property must always return False.</para>
-        /// <para> A <see cref="NotImplementedException" /> exception must be thrown if <see cref="TempCompAvailable" /> is False and an attempt is made to set <see cref="TempComp" /> to true.</para>
-        /// <para><b>BEHAVIOURAL CHANGE - Platform 6.4</b></para>
-        /// <para>Prior to Platform 6.4, the interface specification mandated that drivers must throw an <see cref="InvalidOperationException"/> if a move was attempted when <see cref="TempComp"/> was True, even if the focuser 
-        /// was able to execute the move safely without disrupting temperature compensation.</para>
-        /// <para>Following discussion on ASCOM-Talk in January 2018, the Focuser interface specification has been revised to IFocuserV3, removing the requirement to throw the InvalidOperationException exception. IFocuserV3 compliant drivers 
-        /// are expected to execute Move requests when temperature compensation is active and to hide any specific actions required by the hardware from the client. For example this could be achieved by disabling temperature compensation, moving the focuser and re-enabling 
-        /// temperature compensation or simply by moving the focuser with compensation enabled if the hardware supports this.</para>
-        /// <para>Conform will continue to pass IFocuserV2 drivers that throw InvalidOperationException exceptions. However, Conform will now fail IFocuserV3 drivers that throw InvalidOperationException exceptions, in line with this revised specification.</para>
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.TempComp">Canonical definition</see></remarks>
         bool TempComp { get; set; }
 
         /// <summary>
@@ -132,10 +94,7 @@
         /// </summary>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks>
-        /// <p style="color:red"><b>Must be implemented</b></p>
-        /// Will be True only if the focuser's temperature compensation can be turned on and off via the <see cref="TempComp" /> property. 
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.TempCompAvailable">Canonical definition</see></remarks>
         bool TempCompAvailable { get; }
 
         /// <summary>
@@ -144,9 +103,7 @@
         /// <exception cref="NotImplementedException">If the property is not available for this device.</exception>
         /// <exception cref="NotConnectedException">When <see cref="IAscomDevice.Connected"/> is False.</exception>
         /// <exception cref="DriverException">An error occurred that is not described by one of the more specific ASCOM exceptions. Include sufficient detail in the message text to enable the issue to be accurately diagnosed by someone other than yourself.</exception>
-        /// <remarks><p style="color:red"><b>Can throw a not implemented exception</b></p> 
-        /// Raises an exception if ambient temperature is not available. Commonly available on focusers with a built-in temperature compensation mode. 
-        /// </remarks>
+        /// <remarks>See this link for the canonical definition, which may include further information: <see href="https://ascom-standards.org/newdocs/focuser.html#Focuser.Temperature">Canonical definition</see></remarks>
         double Temperature { get; }
     }
 }
