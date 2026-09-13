afff74709a1c78ed443f1e53567dd70a7d13f9c7
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-09-13 11:35:16 +0100

    Alpaca device - Fix bug in responder that prevented it from listening on the IPv6 localhost address ::1 when instructed to do so. Remove an unnecessary call to UDPClient.BeginReceive at the end  of the try block because the same call is present in the finally block. This avoids an ever escalating number of callbacks for incoming discovery packets and keeps the number of callbacks at 1.

--- ASCOM.Alpaca.Device/Discovery/Responder.cs
+++ ASCOM.Alpaca.Device/Discovery/Responder.cs
@@ -90,6 +90,8 @@ namespace ASCOM.Alpaca.Discovery
         public Responder(int AlpacaPort, int DiscoveryPort, bool IPv4, bool IPv6, ILogger Logger = null)
         {
             this.Logger = Logger;
+
+            Logger?.LogDebug($"Responder.ctor - Creating discovery responder with AlpacaPort: {AlpacaPort}, DiscoveryPort: {DiscoveryPort}, IPv4: {IPv4}, IPv6: {IPv6}");
             port = AlpacaPort;
             this.DiscoveryPort = DiscoveryPort;
 
@@ -110,10 +112,11 @@ namespace ASCOM.Alpaca.Discovery
         }
 
         /// <summary>
-        /// Create and listen on an IPv4 broadcast port
+        /// Create a UDP client that listens on the discovery port of all IPv4 addresses for discovery broadcasts.
         /// </summary>
         private void InitIPv4()
         {
+            Logger?.LogDebug($"Responder.InitIPv4 - Binding to IPv4 Discovery Port: {DiscoveryPort}");
             UdpClient UDPClient = new UdpClient();
 
             UDPClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
@@ -128,58 +131,82 @@ namespace ASCOM.Alpaca.Discovery
             UDPClient.BeginReceive(ReceiveCallback, UDPClient);
 
             Clients.Add(UDPClient);
+            Logger?.LogInformation($"Responder.InitIPv4 - Added discovery responder client on port: {DiscoveryPort}");
         }
 
         /// <summary>
-        /// Bind a UDP client to each network adapter and set the index and address for multicast
+        /// Bind a UDP client to each network interface and set the index and address for multicast
         /// </summary>
         private void InitIPv6()
         {
+            Logger?.LogDebug($"Responder.InitIPv6 - Binding to IPv6 Discovery Port: {DiscoveryPort}");
+
             // Windows needs to have the IP Address and index set for an IPv6 multicast socket
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
+                // Get all the network interfaces on the system
                 NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+                Logger?.LogDebug($"Responder.InitIPv6 - Running on Windows, found {adapters.Length} network adapters");
 
-                foreach (var adapter in adapters)
+                // Enumerate the network interfaces
+                foreach (NetworkInterface adapter in adapters)
                 {
                     try
                     {
-                        if (adapter.OperationalStatus != OperationalStatus.Up)
+                        // Check if the interface is up and running
+                        if (adapter.OperationalStatus != OperationalStatus.Up) // Interface is not up and running
                             continue;
 
-                        if (adapter.Supports(NetworkInterfaceComponent.IPv6) && adapter.SupportsMulticast)
+                        Logger?.LogDebug($"Responder.InitIPv6 - Found network interface: {adapter.Name}, Type: {adapter.NetworkInterfaceType}, Status: {adapter.OperationalStatus}, Supports IPv6: {adapter.Supports(NetworkInterfaceComponent.IPv6)}, Supports Multicast: {adapter.SupportsMulticast}");
+
+                        // Check if this interface supports IPv6 and multicast
+                        if (adapter.Supports(NetworkInterfaceComponent.IPv6) && adapter.SupportsMulticast) // Interface supports IPv6 and multicast
                         {
+                            // Get the IP properties of the interface and ignore the interface if it does not have any descriptive properties
                             IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
                             if (adapterProperties == null)
                                 continue;
 
+                            // Get the unicast addresses of the interface
                             UnicastIPAddressInformationCollection uniCast = adapterProperties.UnicastAddresses;
+                            Logger?.LogDebug($"Responder.InitIPv6 -   Interface: {adapter.Name} supports IPv6 and Multicast and has {uniCast.Count} unicast addresses");
 
-                            if (uniCast.Count > 0)
+                            // Check whether the interface has any unicast addresses and ignore the interface if it does not
+                            if (uniCast.Count > 0) // Interface has unicast addresses
                             {
+                                // Enumerate the unicast addresses of the interface 
                                 foreach (UnicastIPAddressInformation uni in uniCast)
                                 {
                                     try
                                     {
-                                        if (uni.Address.AddressFamily != AddressFamily.InterNetworkV6)
+                                        Logger?.LogDebug($"Responder.InitIPv6 -   Interface: {adapter.Name} has address: {uni.Address} with address family: {uni.Address.AddressFamily}.");
+
+                                        // Check if the unicast address is an IPv6 address
+                                        if (uni.Address.AddressFamily != AddressFamily.InterNetworkV6) // Unicast address is not an IPv6 address so ignore it
                                             continue;
 
-                                        //Only use LinkLocal or LocalHost addresses
-                                        if (!uni.Address.IsIPv6LinkLocal && uni.Address != IPAddress.Parse("::1"))
-                                            continue;
+                                        Logger?.LogDebug($"Responder.InitIPv6 -   Found IPv6 address: {uni.Address}, LinkLocal: {uni.Address.IsIPv6LinkLocal}, Is localhost: {IPAddress.IsLoopback(uni.Address)}");
 
-                                        Clients.Add(NewClient(uni.Address, adapterProperties.GetIPv6Properties().Index));
+                                        // Check whether the unicast address is a LinkLocal or LocalHost address
+                                        // NOTE - Must test for localhost / loopback first because this address returns IsIPv6LinkLocal = false. The "Conditional logical OR (short circuit)" operator || ensures the logic works as intended.
+                                        if (IPAddress.IsLoopback(uni.Address) || uni.Address.IsIPv6LinkLocal) // The address is a LinkLocal or LocalHost address so process it
+                                        {
+                                            Clients.Add(NewClient(uni.Address, adapterProperties.GetIPv6Properties().Index));
+                                            Logger?.LogInformation($"Responder.InitIPv6 -   Added discovery responder client for IPv6 address: {uni.Address}, Index: {adapterProperties.GetIPv6Properties().Index} on port {DiscoveryPort}");
+                                        }
                                     }
-                                    catch
+                                    catch (Exception ex)
                                     {
+                                        Logger?.LogDebug($"Responder.InitIPv6 - Exception occurred while enumerating addresses: {ex.Message}\r\n{ex}");
                                         //May not have permission to access a specific socket or address
                                     }
                                 }
                             }
                         }
                     }
-                    catch
+                    catch (Exception ex)
                     {
+                        Logger?.LogDebug($"Responder.InitIPv6 - Exception occurred while enumerating adapters: {ex.Message}\r\n{ex}");
                         //May not have permission to access a specific socket or address
                     }
                 }
@@ -189,6 +216,7 @@ namespace ASCOM.Alpaca.Discovery
                 //Linux does not, it handles the binding
                 Clients.Add(NewClient(IPAddress.IPv6Any, 0));
             }
+            Logger?.LogDebug($"Responder.InitIPv6 - Completed binding to IPv6 Discovery Port: {DiscoveryPort}");
         }
 
         private UdpClient NewClient(IPAddress host, int index)
@@ -211,63 +239,88 @@ namespace ASCOM.Alpaca.Discovery
         private void ReceiveCallback(IAsyncResult ar)
         {
             UdpClient udpClient = null;
+            IPEndPoint endpoint = null;
+            bool callbackReenabled = false;
+
             try
             {
                 udpClient = (UdpClient)ar.AsyncState;
 
-                IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, DiscoveryPort);
+                endpoint = new IPEndPoint(IPAddress.Any, DiscoveryPort);
 
-                // Obtain the UDP message body and convert it to a string, with remote IP address attached as well
-                string ReceiveString = Encoding.ASCII.GetString(udpClient.EndReceive(ar, ref endpoint));
+                // Consume the completed receive before starting another one because UdpClient reuses its receive buffer.
+                byte[] receivedBytes = udpClient.EndReceive(ar, ref endpoint);
 
+                // Configure the UdpClient class to accept more messages while this message is being processed.
+                udpClient.BeginReceive(ReceiveCallback, udpClient);
+                callbackReenabled = true;
+
+                // Convert the UDP message body to a string
+                string ReceiveString = Encoding.ASCII.GetString(receivedBytes);
+
+                // Default discovery to disallowed, then check the address and configuration to see if it is allowed
                 bool discoveryAllowed = false;
 
-                if (IPAddress.IsLoopback(endpoint.Address))
+                // Check for loopback address
+                if (IPAddress.IsLoopback(endpoint.Address)) // Loopback addresses are always allowed
                 {
                     discoveryAllowed = true;
                 }
-                else if (IsLocalIpAddress(endpoint.Address.ToString()))
+
+                // Check for local address
+                else if (IsLocalIpAddress(endpoint.Address.ToString())) // This is a local address
                 {
-                    if (!LocalRespondOnlyToLocalHost)
+                    // Check if we are configured to only respond to local host addresses
+                    if (!LocalRespondOnlyToLocalHost) // We are responding on all addresses
                     {
                         discoveryAllowed = true;
                     }
                 }
-                else if (AllowRemoteAccess)
+
+                // Check if we are configured to allow remote access
+                else if (AllowRemoteAccess) // We are configured to allow remote access, so allow discovery from this address
                 {
                     discoveryAllowed = true;
                 }
 
-                if (discoveryAllowed)
+                // Check if discovery is allowed
+                if (discoveryAllowed) // Discovery is allowed
                 {
-                    if (ReceiveString.Contains(Constants.DiscoveryMessage))//Contains rather then equals because of invisible padding garbage
+                    // Check whether the message is a discovery message
+                    if (ReceiveString.Contains(Constants.DiscoveryMessage)) // This is a discovery message - NOTE: Uses Contains rather then Equals because of invisible padding garbage
                     {
                         //For testing only
-                        Logger?.LogInformation(string.Format("Received a discovery packet from {0} at {1}", endpoint.Address, DateTime.Now));
+                        Logger?.LogInformation($"Responding to a discovery packet from {endpoint.Address} at {DateTime.Now}");
 
-                        byte[] response = Encoding.ASCII.GetBytes(string.Format("{{\"AlpacaPort\": {0}}}", port));
+                        // Create a response message containing the Alpaca port number
+                        byte[] response = Encoding.ASCII.GetBytes($"{{\"AlpacaPort\": {port}}}");
 
+                        // Send the response message back to the originator of the discovery message
                         udpClient.Send(response, response.Length, endpoint);
                     }
                 }
-
-                // Configure the UdpClient class to accept more messages, if they arrive
-                udpClient.BeginReceive(ReceiveCallback, udpClient);
+                else // Discovery is not allowed so log to debug so we know what is happening
+                {
+                    Logger?.LogDebug($"Ignoring discovery request from {endpoint.Address} because it is not a loopback address and configuration is: Only respond on localhost for local addresses: {LocalRespondOnlyToLocalHost}, AllowRemoteAccess: {AllowRemoteAccess}.");
+                }
             }
             catch (Exception ex)
             {
-                Logger?.LogError(ex.Message);
+                Logger?.LogError($"Responder.ReceiveCallback - Error processing discovery request from {endpoint.Address}: {ex.Message}");
             }
             finally
             {
-                try
+                if (!callbackReenabled)
                 {
-                    // Configure the UdpClient class to accept more messages, if they arrive
-                    udpClient.BeginReceive(ReceiveCallback, udpClient);
-                }
-                catch (Exception ex)
-                {
-                    Logger?.LogError(ex.Message);
+                    try
+                    {
+                        // If processing failed before the normal restart, continue listening where possible.
+                        udpClient?.BeginReceive(ReceiveCallback, udpClient);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger?.LogError($"Responder.ReceiveCallback - Unable to continue listening for discovery broadcasts: {ex.Message}");
+                    }
                 }
             }
         }
@@ -308,7 +361,7 @@ namespace ASCOM.Alpaca.Discovery
             {
                 if (disposing)
                 {
-                    foreach (var udp in Clients)
+                    foreach (UdpClient udp in Clients)
                     {
                         try
                         {
--- ASCOM.Alpaca.Device/README.md
+++ ASCOM.Alpaca.Device/README.md
@@ -5,6 +5,9 @@ Library of ASCOM Alpaca device specific components.
 ## Version History
 The version history only contains entries when a change is made.
 
+***Release 4.1.0***
+* BUGFIX - The responder now listens for discovery packets on the IPv6 localhost address ::1 when configured to do so. Previously it only listened on the IPv4 localhost address.
+
 ***Release 3.0.0***
 *  ADDED - Support for .NET 8.0, 9.0 and 10.0 target frameworks. It will no longer be necessary for applications that use these frameworks to 
 rely on the .NET Standard 2.0 component.
