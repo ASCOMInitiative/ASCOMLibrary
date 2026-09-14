ee15c6700a9ba7ae20defc0409bff75db733ef73
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-09-14 13:56:34 +0100

    Update build number tests with latest version numbers.

--- UnitTests/PlatformUtilities/BuildNumberTests.cs
+++ UnitTests/PlatformUtilities/BuildNumberTests.cs
@@ -19,15 +19,23 @@ namespace PlatformUtilitiesTests
         {
             Assert.Equal("Unknown OS version (0)", ASCOM.Com.PlatformUtilities.OSBuildName(0));
         }
+
         [Fact]
         public void Build_5000()
         {
             Assert.Equal("Earlier than Windows 10 (build < 10000)", ASCOM.Com.PlatformUtilities.OSBuildName(5000));
         }
+        
         [Fact]
         public void Build_26200()
         {
             Assert.Equal("Windows 11 (25H2)", ASCOM.Com.PlatformUtilities.OSBuildName(26200));
+        }
+        
+        [Fact]
+        public void Build_26300()
+        {
+            Assert.Equal($"Windows 11 (26H2)", ASCOM.Com.PlatformUtilities.OSBuildName(26300));
         }
         [Fact]
         public void Build_28000()
@@ -35,22 +43,28 @@ namespace PlatformUtilitiesTests
             Assert.Equal("Windows 11 (26H1)", ASCOM.Com.PlatformUtilities.OSBuildName(28000));
         }
 
-        const string lastKnownBuildName = "Windows 11 (26H1)";
-
         [Fact]
         public void Build_28020()
         {
-            Assert.Equal($"{lastKnownBuildName} or later", ASCOM.Com.PlatformUtilities.OSBuildName(28020));
+            Assert.Equal($"Windows 11 (26H1)", ASCOM.Com.PlatformUtilities.OSBuildName(28020));
         }
+        
         [Fact]
         public void Build_28100()
         {
-            Assert.Equal($"{lastKnownBuildName} or later", ASCOM.Com.PlatformUtilities.OSBuildName(28100));
+            Assert.Equal($"Windows 11 (26H1)", ASCOM.Com.PlatformUtilities.OSBuildName(28100));
         }
+
+        [Fact]
+        public void Build_29020()
+        {
+            Assert.Equal($"Windows 11 (27H2)", ASCOM.Com.PlatformUtilities.OSBuildName(29020));
+        }
+
         [Fact]
         public void Build_99999()
         {
-            Assert.Equal($"{lastKnownBuildName} or later", ASCOM.Com.PlatformUtilities.OSBuildName(99999));
+            Assert.Equal($"Windows 11 (27H2) or later", ASCOM.Com.PlatformUtilities.OSBuildName(99999));
         }
     }
 }
